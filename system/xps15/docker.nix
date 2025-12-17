4f59bf7fae1f5404306f16d90fe0c4648d93101e
h3xit <connect@h3x.it>
2025-12-16 23:31:00 -0500

    Add docker

---
+++ system/xps15/docker.nix
@@ -0,0 +1,5 @@
+{...}: {
+  virtualisation.docker.enable = true;
+  #virtualisation.docker.extraOptions = "--insecure-registry 'http://192.168.0.99:5000'";
+  users.extraGroups.docker.members = ["h3x"];
+}
