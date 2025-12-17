966c80d1996e0574d4ee34e51d45ba3dcc9400c9
h3xit <connect@h3x.it>
2025-12-16 22:02:42 -0500

    macbook config

--- system/macbook/configuration.nix
+++ system/macbook/configuration.nix
@@ -1,23 +1,21 @@
 { pkgs, ... }:
 {
-      environment.systemPackages  = with pkgs; [
-            vim
-            vscode
-            discord
-      ];
-      nixpkgs.config.allowUnfree = true;
-      nix.settings.experimental-features = "nix-command flakes";
-      nix.settings.substituters = [ "https://cache.nixos.org" ];
-      nix.settings.trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
-      programs.zsh.enable = true;
+  environment.systemPackages  = with pkgs; [
+        vim
+        vscode
+        discord
+  ];
 
-      # Set Git commit hash for darwin-version.
-      #system.configurationRevision = self.rev or self.dirtyRev or null;
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = "nix-command flakes";
+  nix.settings.substituters = [ "https://cache.nixos.org" ];
+  nix.settings.trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" ];
+  programs.zsh.enable = true;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 6;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
 }
