061a76320f4a15129c8ae268a3ec5567e7e69cab
h3xit <h3xit@protonmail.com>
2025-12-23 22:19:02 -0500

    Removed libvirtd group

--- system/xps15/libvirtd-virtmanager.nix
+++ system/xps15/libvirtd-virtmanager.nix
@@ -1,9 +1,11 @@
 {pkgs, ...}: {
   virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd.qemu.swtpm.enable = true;
   programs.virt-manager.enable = true;
-  users.extraGroups.libvirtd.members = ["h3x"];
+  users.extraGroups.libvirt.members = ["h3x"];
   environment.systemPackages = with pkgs; [
     virtiofsd
     virglrenderer
   ];
+  environment.variables.LIBVIRT_DEFAULT_URI = "qemu:///system";
 }
