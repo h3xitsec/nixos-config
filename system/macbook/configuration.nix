1f8ba7b10c4286608240f60897dd6146fd54f7f0
h3xit <connect@h3x.it>
2025-12-26 23:44:01 -0500

    darwin add app + split files

--- system/macbook/configuration.nix
+++ system/macbook/configuration.nix
@@ -2,36 +2,17 @@
 {
   imports = [
     ./users.nix
+    ./dock.nix
+    ./homebrew.nix
     # Import shared modules
     ../../lib/shared/nix-settings.nix
   ];
   system.primaryUser = "h3x";
   system.defaults = {
-    dock = {
-      autohide = false;
-      orientation = "bottom";
-      show-process-indicators = false;
-      show-recents = false;
-      persistent-apps = [
-        "/System/Cryptexes/App/System/Applications/Safari.app"
-        "/System/Applications/Mail.app"
-        "/System/Applications/Messages.app"
-        "/System/Applications/Music.app"
-        "/System/Applications/TV.app"
-        "/Applications/Nix Apps/iTerm2.app"
-        "/Users/h3x/Applications/Home Manager Apps/Visual Studio Code.app"
-        "/Applications/BambuStudio.app"
-        "/Applications/Caido.app"
-        "/Applications/1Password.app"
-        "/Users/h3x/Applications/Autodesk Fusion.app"
-      ];
-    };
-    # Other system settings...
     loginwindow = {
       GuestEnabled = false;
       DisableConsoleAccess = true;
     };
-
     finder = {
       AppleShowAllFiles = true; # hidden files
       AppleShowAllExtensions = true; # file extensions
@@ -39,7 +20,6 @@
       ShowPathbar = true; # breadcrumb nav at bottom
       ShowStatusBar = true; # file count & disk space
     };
-
     NSGlobalDomain = {
       NSAutomaticSpellingCorrectionEnabled = false;
       NSAutomaticCapitalizationEnabled = false;
@@ -52,19 +32,7 @@
     discord
     iterm2
   ];
-  homebrew = {
-    enable = true;
-    onActivation.cleanup = "uninstall";
-    taps = [];
-    brews = [];
-    casks = [
-      "caffeine"
-      "caido"
-      "1password"
-      "logi-options+"
-      "autodesk-fusion"
-    ];
-  };
+  
   programs.zsh.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
---
+++ system/macbook/homebrew.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+{
+  homebrew = {
+    enable = true;
+    onActivation.cleanup = "uninstall";
+    taps = [];
+    brews = [];
+    casks = [
+      "caffeine"
+      "caido"
+      "1password"
+      "logi-options+"
+      "autodesk-fusion"
+      "parsec"
+      "bambu-studio"
+    ];
+  };
+}
