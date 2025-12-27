fdcbfbecdac992371e9a28b17df2c0b3c42ea9b9
h3xit <h3xit@protonmail.com>
2025-12-26 23:23:53 -0500

    Fixed GTK theme errors at login

--- home/xps15/theme.nix
+++ home/xps15/theme.nix
@@ -1,10 +1,18 @@
 {pkgs, ...}: {
+  # Use dconf to set color scheme for libadwaita/GTK4 apps
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+
+  # Install theme packages (GTK3 needs gnome-themes-extra for Adwaita-dark)
+  home.packages = [ pkgs.gnome-themes-extra ];
+
   gtk = {
     enable = true;
-    theme = {
-      name = "adwaita-dark";
-      package = pkgs.gnome-themes-extra;
-    };
+    # Don't set gtk.theme - it writes to both GTK3 and GTK4 configs,
+    # but gnome-themes-extra has no GTK4 themes (GTK4 uses built-in Adwaita)
     iconTheme = {
       name = "Adwaita";
       package = pkgs.adwaita-icon-theme;
@@ -13,12 +21,12 @@
       name = "Adwaita";
       package = pkgs.adwaita-icon-theme;
     };
+    # GTK3: set theme and dark preference
     gtk3.extraConfig = {
+      gtk-theme-name = "Adwaita-dark";
       gtk-application-prefer-dark-theme = true;
     };
-    gtk4.extraConfig = {
-      gtk-application-prefer-dark-theme = true;
-    };
+    # GTK4: uses built-in Adwaita + color-scheme from dconf for dark mode
   };
 
   qt = {
