17f36efdd8c342dbf0cfcf475bdbf47389b7b9d0
h3xit <h3xit@protonmail.com>
2026-01-17 21:18:25 -0500

    Flake update + removed deprecated setting

--- hosts/h3xlptp/modules/services.nix
+++ hosts/h3xlptp/modules/services.nix
@@ -9,14 +9,6 @@
   # DNS-over-TLS configuration
   services.resolved = {
     enable = true;
-    extraConfig = ''
-      DNS=1.1.1.1 8.8.8.8
-      FallbackDNS=1.0.0.1 8.8.4.4
-      DNSOverTLS=yes
-      Cache=yes
-      DNSStubListener=yes
-      Domains=~.
-    '';
   };
 
   # Tailscale VPN
