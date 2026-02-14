77882128f5d3ca57d228727b75d13e2282baa9ac
h3xit <connect@h3x.it>
2026-02-14 17:28:35 -0500

    Added more MacOS config

--- hosts/h3xmac/configuration.nix
+++ hosts/h3xmac/configuration.nix
@@ -3,31 +3,131 @@
   username,
   ...
 }: {
-  system.primaryUser = username;
-  system.defaults = {
-    loginwindow = {
-      GuestEnabled = false;
-      DisableConsoleAccess = true;
+  system = {
+    primaryUser = username;
+    defaults = {
+      loginwindow = {
+        GuestEnabled = false;
+        DisableConsoleAccess = true;
+        SHOWFULLNAME = true;
+      };
+      finder = {
+        AppleShowAllFiles = true; # hidden files
+        AppleShowAllExtensions = true; # file extensions
+        _FXShowPosixPathInTitle = true; # title bar full path
+        ShowPathbar = true; # breadcrumb nav at bottom
+        ShowStatusBar = true; # file count & disk space
+      };
+      trackpad = {
+        Clicking = true;  # enable tap to click
+        TrackpadRightClick = true;  # enable two finger right click
+        TrackpadThreeFingerDrag = true;  # enable three finger drag
+      };
+      NSGlobalDomain = {
+        # `defaults read NSGlobalDomain "xxx"`
+        "com.apple.swipescrolldirection" = true;  # enable natural scrolling(default to true)
+        "com.apple.sound.beep.feedback" = 0;  # disable beep sound when pressing volume up/down key
+        AppleInterfaceStyle = "Dark";  # dark mode
+        AppleKeyboardUIMode = 3;  # Mode 3 enables full keyboard control.
+        ApplePressAndHoldEnabled = true;  # enable press and hold
+
+        # If you press and hold certain keyboard keys when in a text area, the key’s character begins to repeat.
+        # This is very useful for vim users, they use `hjkl` to move cursor.
+        # sets how long it takes before it starts repeating.
+        InitialKeyRepeat = 15;  # normal minimum is 15 (225 ms), maximum is 120 (1800 ms)
+        # sets how fast it repeats once it starts. 
+        KeyRepeat = 3;  # normal minimum is 2 (30 ms), maximum is 120 (1800 ms)
+
+        NSAutomaticCapitalizationEnabled = false;  # disable auto capitalization
+        NSAutomaticDashSubstitutionEnabled = false;  # disable auto dash substitution
+        NSAutomaticPeriodSubstitutionEnabled = false;  # disable auto period substitution
+        NSAutomaticQuoteSubstitutionEnabled = false;  # disable auto quote substitution
+        NSAutomaticSpellingCorrectionEnabled = false;  # disable auto spelling correction
+        NSNavPanelExpandedStateForSaveMode = true;  # expand save panel by default
+        NSNavPanelExpandedStateForSaveMode2 = true;
+        NSAutomaticWindowAnimationsEnabled = false;
+      };
+      CustomUserPreferences = {
+        ".GlobalPreferences" = {
+          # automatically switch to a new space when switching to the application
+          AppleSpacesSwitchOnActivate = true;
+        };
+        NSGlobalDomain = {
+          # Add a context menu item for showing the Web Inspector in web views
+          WebKitDeveloperExtras = true;
+        };
+        "com.apple.finder" = {
+          ShowExternalHardDrivesOnDesktop = true;
+          ShowHardDrivesOnDesktop = true;
+          ShowMountedServersOnDesktop = true;
+          ShowRemovableMediaOnDesktop = true;
+          _FXSortFoldersFirst = true;
+          # When performing a search, search the current folder by default
+          FXDefaultSearchScope = "SCcf";
+        };
+        "com.apple.desktopservices" = {
+          # Avoid creating .DS_Store files on network or USB volumes
+          DSDontWriteNetworkStores = true;
+          DSDontWriteUSBStores = true;
+        };
+        "com.apple.spaces" = {
+          "spans-displays" = 0; # Display have seperate spaces
+        };
+        "com.apple.WindowManager" = {
+          EnableStandardClickToShowDesktop = 0; # Click wallpaper to reveal desktop
+          StandardHideDesktopIcons = 0; # Show items on desktop
+          HideDesktop = 0; # Do not hide items on desktop & stage manager
+          StageManagerHideWidgets = 0;
+          StandardHideWidgets = 0;
+        };
+        "com.apple.screensaver" = {
+          # Require password immediately after sleep or screen saver begins
+          askForPassword = 1;
+          askForPasswordDelay = 0;
+        };
+        "com.apple.screencapture" = {
+          location = "~/Desktop";
+          type = "png";
+        };
+        "com.apple.AdLib" = {
+          allowApplePersonalizedAdvertising = false;
+        };
+        # Prevent Photos from opening automatically when devices are plugged in
+        "com.apple.ImageCapture".disableHotPlug = true;
+      };
     };
-    finder = {
-      AppleShowAllFiles = true; # hidden files
-      AppleShowAllExtensions = true; # file extensions
-      _FXShowPosixPathInTitle = true; # title bar full path
-      ShowPathbar = true; # breadcrumb nav at bottom
-      ShowStatusBar = true; # file count & disk space
-    };
-    NSGlobalDomain = {
-      NSAutomaticSpellingCorrectionEnabled = false;
-      NSAutomaticCapitalizationEnabled = false;
-      NSAutomaticPeriodSubstitutionEnabled = false;
-      NSAutomaticWindowAnimationsEnabled = false;
+    keyboard = {
+      enableKeyMapping = true;  # enable key mapping so that we can use `option` as `control`
+
+      # NOTE: do NOT support remap capslock to both control and escape at the same time
+      remapCapsLockToControl = false;  # remap caps lock to control, useful for emac users
+      remapCapsLockToEscape  = false;   # remap caps lock to escape, useful for vim users
+
+      # swap left command and left alt 
+      # so it matches common keyboard layout: `ctrl | command | alt`
+      #
+      # disabled, caused only problems!
+      swapLeftCommandAndLeftAlt = false;  
     };
   };
   environment.systemPackages = with pkgs; [
     vim
     iterm2
   ];
+  fonts = {
+    packages = with pkgs; [
+      # icon fonts
+      material-design-icons
+      font-awesome
 
+      # nerdfonts
+      # https://github.com/NixOS/nixpkgs/blob/nixos-unstable-small/pkgs/data/fonts/nerd-fonts/manifests/fonts.json
+      nerd-fonts.symbols-only # symbols icon only
+      nerd-fonts.fira-code
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.iosevka
+    ];
+  };
   programs.zsh.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
