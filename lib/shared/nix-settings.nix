f304e7a428f525fe210c14ec5906b4596e181a32
h3xit <h3xit@protonmail.com>
2026-05-16 23:00:09 -0400

    Added cache for cuda packages

--- lib/shared/nix-settings.nix
+++ lib/shared/nix-settings.nix
@@ -20,6 +20,7 @@
         "https://niri.cachix.org"
         "https://nixpkgs-wayland.cachix.org"
         "https://devenv.cachix.org"
+        "https://cache.nixos-cuda.org"
       ];
 
       trusted-public-keys = [
@@ -28,6 +29,7 @@
         "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
         "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       ];
 
       # Enable experimental features
